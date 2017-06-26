f48487603669a0bb3694bbedbe906203c0ad7cbb
Tony Prior <tony@gaiaresources.com.au>
2017-06-26 11:12:29 +0800

    Another logout fix attempt, by having it redirect to OIM logout page upon logging out.
    Fixed up record geometry editing to use dataset datum/zone rather than record zone by default.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     server: 'https://biosys.dpaw.wa.gov.au',
-    apiExtension: '/api/'
+    apiExtension: '/api/',
+    logoutUrl: 'https://oim.dpaw.wa.gov.au/logout'
 };
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     server: 'https://biosys-uat.dpaw.wa.gov.au',
-    apiExtension: '/api/'
+    apiExtension: '/api/',
+    logoutUrl: 'https://oim.dpaw.wa.gov.au/logout'
 };
