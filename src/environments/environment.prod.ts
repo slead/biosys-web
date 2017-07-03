90fc4afbc779dcb56aeb345543e54fb519b490cb
Tony Prior <tony@gaiaresources.com.au>
2017-07-03 12:09:38 +0800

    Changed api endpoint for UAT/production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     server: 'https://biosys.dpaw.wa.gov.au',
-    apiExtension: '/api/',
+    apiExtension: '/sso-api/',
     logoutUrl: 'https://oim.dpaw.wa.gov.au/logout',
     cookieAuthToken: 'oim_dpaw_wa_gov_au_sessionid'
 };
