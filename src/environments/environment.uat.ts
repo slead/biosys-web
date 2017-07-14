d2470a5c027a1e3c6564c0f95dc961bd5bae82dd
tony-gaia <tony@gaiaresources.com.au>
2017-07-14 14:50:34 +0800

    Updated cookie name

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -3,5 +3,5 @@ export const environment = {
     server: 'https://biosys-uat.dbca.wa.gov.au',
     apiExtension: '/sso-api/',
     logoutUrl: 'https://biosys-uat.dbca.wa.gov.au/sso/auth_logout',
-    cookieAuthToken: 'oim_dpaw_wa_gov_au_sessionid'
+    cookieAuthToken: 'dbca_wa_gov_au_sessionid'
 };
