485849828ed6556db5ca2aafbca7aacde8bcdebf
Tony Prior <tony@gaiaresources.com.au>
2018-01-22 12:54:06 +0800

    Updated fileuploader to use a more recent template from primeng. Ability to show/hide cancel button now contained in primeng version so removed from custom.

--- src/app/shared/fileuploader/fileuploader.component.ts
+++ src/app/shared/fileuploader/fileuploader.component.ts
@@ -9,12 +9,4 @@ import { FileUpload, DomHandler } from 'primeng/primeng';
 export class FileuploaderComponent extends FileUpload {
     @Input()
     public loading: boolean;
-
-    @Input()
-    public cancellable: boolean = true;
-
-    public onChooseClick(event, fileInput) {
-        fileInput.value = null;
-        fileInput.click();
-    }
 }
